c58a03607da09d3160b29b6aab9802acbdd47742
Fomin Alexey <fomin.alexey@outlook.com>
2024-06-22 14:56:00 +0300

    add diagram

--- code/animal_accounting_system/src/Animal/Animal.java
+++ code/animal_accounting_system/src/Animal/Animal.java
@@ -1,4 +1,13 @@
 package Animal;
 
-public class Animal {
+import java.util.Date;
+
+abstract class Animal {
+    private String name;
+    private Date dateofbirth;
+
+    public Animal(String name, Date dateofbirth) {
+        this.name = name;
+        this.dateofbirth = dateofbirth;
+    }
 }
