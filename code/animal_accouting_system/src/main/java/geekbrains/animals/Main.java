a552ac669bf614ddb095a7d6c86e5ccf94173e46
Fomin Alexey <fomin.alexey@outlook.com>
2024-06-25 12:17:24 +0300

    choose case

--- code/animal_accouting_system/src/main/java/geekbrains/animals/Main.java
+++ code/animal_accouting_system/src/main/java/geekbrains/animals/Main.java
@@ -11,37 +11,34 @@ import geekbrains.animals.Model.Utils.Files;
 import geekbrains.animals.View.Cmd.Cmd;
 import geekbrains.animals.View.Cmd.Style;
 import geekbrains.animals.View.UI.Menu;
+import geekbrains.animals.View.Wellcome;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args)  {
 
 
-        Repository repository = Repository.loadFromFile(Config.RepositoryFilePath);
+        Wellcome.Run();
 
-//        Repository repository = new Repository();
-//        Cat cat = new Cat("sunny", LocalDate.of(1988, 10, 4));
-//        Dog dog = new Dog("bob", LocalDate.of(2014, 9, 18));
-//        Hamster hamster = new Hamster("jemmy", LocalDate.of(2023, 7, 5));
-//        Horse horse = new Horse("german", LocalDate.of(2000, 5, 5));
+//        Repository repository = Repository.loadFromFile(Config.RepositoryFilePath);
+//
+////        Repository repository = new Repository();
+////        Cat cat = new Cat("sunny", LocalDate.of(1988, 10, 4));
+////        Dog dog = new Dog("bob", LocalDate.of(2014, 9, 18));
+////        Hamster hamster = new Hamster("jemmy", LocalDate.of(2023, 7, 5));
+////        Horse horse = new Horse("german", LocalDate.of(2000, 5, 5));
+//
+//
+//
+////        repository.addAnimals(cat, dog, hamster, horse);
+//        repository.printListOfAnimals();
+////        repository.saveToFile(Config.RepositoryFilePath);
 
 
 
-//        repository.addAnimals(cat, dog, hamster, horse);
-        repository.printListOfAnimals();
-//        repository.saveToFile(Config.RepositoryFilePath);
-
-
-        Menu menu = new Menu();
-        menu.showTitle("Вы хотите выбрать купить наш товар?");
-        menu.showDescription("Для того чтобы получить много текста, нам проиходится его разбивать на различные строки, таким образом все получается", 60);
-        System.out.println();
-        menu.addPoint('1', "first point");
-        menu.addPoint('2', "second point");
-
-        menu.showMenu();
 
 
     }
